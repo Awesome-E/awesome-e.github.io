b0b1e454abc829d142311c726556860e0a942f1f
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2024-04-18 16:47:41 +0000

    Deploying to gh-pages from @ Awesome-E/awesome-e.github.io@ed119da264cf05187b7a74ba81f7d15e9e5902fa 🚀

--- index.94d5b1c8.js
+++
@@ -1,4 +0,0 @@
-document.body.ontouchstart=()=>{};const e=document.querySelectorAll(".sidenav, .overlay");e.forEach(e=>e.addEventListener("touchmove",t=>{// Prevent Scrolling for now
-    e.scrollHeight<=e.offsetHeight&&t.preventDefault()}));//# sourceMappingURL=index.94d5b1c8.js.map
-
-//# sourceMappingURL=index.94d5b1c8.js.map
--- index.f1147326.js
+++ index.f1147326.js
@@ -1,2 +1,2 @@
 document.body.ontouchstart=()=>{},document.querySelectorAll(".sidenav, .overlay").forEach(e=>e.addEventListener("touchmove",t=>{e.scrollHeight<=e.offsetHeight&&t.preventDefault()}));
-//# sourceMappingURL=main.js.map
+//# sourceMappingURL=index.f1147326.js.map
